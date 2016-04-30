b7c2f4aa198a18ddb3603850b1bcdc38212dc4b8
mfenix360 <dariosuarez_@hotmail.com>
2016-04-30 12:16:26 +0200

    cambios

--- VoteCounting_2a/src/test/java/es/uniovi/asw/rest/VotoRestTest.java
+++ VoteCounting_2a/src/test/java/es/uniovi/asw/rest/VotoRestTest.java
@@ -43,25 +43,25 @@ public class VotoRestTest {
 		vr.setColegioElectoral("Colegio1");
 		assertTrue(vr.getColegioElectoral().equals("Colegio1"));
 		//assertFalse(vr.getColegioElectoral().equals(null));
-		assertNotNull(vr.getEleccion());
+		assertNull(vr.getEleccion());
 		
 		vr.setColegioElectoral(null);
 		assertTrue(vr.getColegioElectoral().equals("Colegio1"));
 		//assertFalse(vr.getColegioElectoral().equals(null));
-		assertNotNull(vr.getEleccion());
+		assertNull(vr.getEleccion());
 		
 		vr.setColegioElectoral("Colegio2");
 		assertTrue(vr.getColegioElectoral().equals("Colegio2"));
 		assertFalse(vr.getColegioElectoral().equals("Colegio1"));
 		//assertFalse(vr.getColegioElectoral().equals(null));
-		assertNotNull(vr.getEleccion());
+		assertNull(vr.getEleccion());
 		
 		vr.setColegioElectoral("");
 		assertFalse(vr.getColegioElectoral().equals(""));
 		assertTrue(vr.getColegioElectoral().equals("Colegio2"));
 		assertFalse(vr.getColegioElectoral().equals("Colegio1"));
 		//assertFalse(vr.getColegioElectoral().equals(null));
-		assertNotNull(vr.getEleccion());
+		assertNull(vr.getEleccion());
 		
 	}
 
@@ -71,25 +71,25 @@ public class VotoRestTest {
 		vr.setOpcion("No");
 		assertTrue(vr.getOpcion().equals("No"));
 		//assertFalse(vr.getOpcion().equals(null));
-		assertNotNull(vr.getEleccion());
+		assertNull(vr.getEleccion());
 
 		vr.setOpcion(null);
 		assertTrue(vr.getOpcion().equals("No"));
 		//assertFalse(vr.getOpcion().equals(null));
-		assertNotNull(vr.getEleccion());
+		assertNull(vr.getEleccion());
 		
 		vr.setOpcion("Si");
 		assertTrue(vr.getOpcion().equals("Si"));
 		assertFalse(vr.getOpcion().equals("No"));
 		//assertFalse(vr.getOpcion().equals(null));
-		assertNotNull(vr.getEleccion());
+		assertNull(vr.getEleccion());
 		
 		vr.setOpcion("");
 		assertFalse(vr.getOpcion().equals(""));
 		assertTrue(vr.getOpcion().equals("Si"));
 		assertFalse(vr.getOpcion().equals("No"));
 		//assertFalse(vr.getOpcion().equals(null));
-		assertNotNull(vr.getEleccion());
+		assertNull(vr.getEleccion());
 		
 	}
 
@@ -110,19 +110,19 @@ public class VotoRestTest {
 		assertTrue(vr1.getEleccion().equals("1"));
 		assertFalse(vr1.getEleccion().equals("4"));
 		//assertFalse(vr1.getEleccion().equals(null));
-		assertNotNull(vr.getEleccion());
+		assertNull(vr.getEleccion());
 		assertFalse(vr1.getEleccion().equals(""));
 
 		assertTrue(vr1.getColegioElectoral().equals("1"));
 		assertFalse(vr1.getColegioElectoral().equals("2"));
 		//assertFalse(vr1.getColegioElectoral().equals(null));
-		assertNotNull(vr.getEleccion());
+		assertNull(vr.getEleccion());
 		assertFalse(vr1.getColegioElectoral().equals(""));
 
 		assertTrue(vr1.getOpcion().equals("Si"));
 		assertFalse(vr1.getOpcion().equals("No"));
 		//assertFalse(vr1.getOpcion().equals(null));
-		assertNotNull(vr.getEleccion());
+		assertNull(vr.getEleccion());
 		assertFalse(vr1.getOpcion().equals(""));
 
 		assertTrue(vr1.isOnline());
