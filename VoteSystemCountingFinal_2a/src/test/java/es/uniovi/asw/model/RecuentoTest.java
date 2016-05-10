b24475da1448c11ce43a0ab0dfc645d7303aef34
mfenix360 <dariosuarez_@hotmail.com>
2016-05-10 11:56:49 +0200

    cambios

--- VoteSystemCountingFinal_2a/src/test/java/es/uniovi/asw/model/RecuentoTest.java
+++ VoteSystemCountingFinal_2a/src/test/java/es/uniovi/asw/model/RecuentoTest.java
@@ -14,7 +14,7 @@ public class RecuentoTest {
 
 		rec.setIdEleccion((long) 3);
 		assertTrue(rec.getIdEleccion().equals((long) 3));
-		assertFalse(rec.getIdEleccion().equals(null));
+		assertFalse(rec.getIdEleccion()==(null));
 
 		rec.setIdEleccion((long) 0);
 		assertFalse(rec.getIdEleccion().equals((long) 0));
@@ -25,7 +25,7 @@ public class RecuentoTest {
 		assertTrue(rec.getIdEleccion().equals((long) 3));
 
 		rec.setIdEleccion(null);
-		assertFalse(rec.getIdEleccion().equals(null));
+		assertFalse(rec.getIdEleccion()==(null));
 		assertTrue(rec.getIdEleccion().equals((long) 3));
 
 	}
@@ -37,19 +37,19 @@ public class RecuentoTest {
 		assertTrue(rec.getOpcion().equals("No"));
 
 		rec.setOpcion(null);
-		assertFalse(rec.getOpcion().equals(null));
+		assertFalse(rec.getOpcion()==(null));
 		assertTrue(rec.getOpcion().equals("No"));
 
 		rec.setOpcion("Si");
 		assertTrue(rec.getOpcion().equals("Si"));
 		assertFalse(rec.getOpcion().equals("No"));
-		assertFalse(rec.getOpcion().equals(null));
+		assertFalse(rec.getOpcion()==(null));
 
 		rec.setOpcion("");
 		assertFalse(rec.getOpcion().equals(""));
 		assertTrue(rec.getOpcion().equals("Si"));
 		assertFalse(rec.getOpcion().equals("No"));
-		assertFalse(rec.getOpcion().equals(null));
+		assertFalse(rec.getOpcion()==(null));
 
 	}
 
@@ -58,7 +58,7 @@ public class RecuentoTest {
 
 		rec.setTotal((long) 60);
 		assertTrue(rec.getTotal().equals((long) 60));
-		assertFalse(rec.getTotal().equals(null));
+		assertFalse(rec.getTotal()==(null));
 
 		rec.setTotal((long) 0);
 		assertTrue(rec.getTotal().equals((long) 0));
@@ -68,7 +68,7 @@ public class RecuentoTest {
 		assertTrue(rec.getTotal().equals((long) 0));
 
 		rec.setTotal(null);
-		assertFalse(rec.getTotal().equals(null));
+		assertFalse(rec.getTotal()==(null));
 		assertTrue(rec.getTotal().equals((long) 0));
 
 	}
